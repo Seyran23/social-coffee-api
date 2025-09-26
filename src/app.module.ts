87b0e5fbfa5b77fffbda5418707c824f2609edfe
Seyran Musayev <s.musayev@andersenlab.com>
2025-09-26 22:14:18 +0200

    chore(prisma): configure Prisma client and module integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
+import { PrismaModule } from './database/prisma.module';
 import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
