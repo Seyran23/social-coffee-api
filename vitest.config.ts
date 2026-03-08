eaa242301ba572d548ef69558f747ea192af847b
Seyran Musayev <seyranm230@gmail.com>
2026-03-08 20:24:28 +0100

    test: migrate testing framework from Jest to Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,41 @@
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    root: './',
+    include: ['**/*.spec.ts', '**/*.e2e-spec.ts'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        '**/*.module.ts',
+        '**/*.dto.ts',
+        '**/*.interface.ts',
+        '**/constants/**',
+        '**/main.ts',
+        '**/*.spec.ts',
+        '**/*.e2e-spec.ts',
+        'dist/**',
+        'prisma/**',
+        'test/**',
+      ],
+      thresholds: {
+        lines: 80,
+        functions: 75,
+        branches: 70,
+        statements: 80,
+      },
+    },
+  },
+  plugins: [
+    tsconfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+});
